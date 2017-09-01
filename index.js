8a3ff5301852fccd1805988256b895d544eaa805
Rachel Bartolo <rachbartolo@gmail.com>
2017-09-01 23:22:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // write your code below!
 var name = "Joe"
 var height = 74
-var message = `${name} is ${height} inches tall`
-var new = `${name} eats ${height} sandwiches weekly`
+var message = `${name} eats ${height} sandwiches weekly`
 
-console.log(new)
+console.log(message)
 
 module.exports = {name, height, message}
